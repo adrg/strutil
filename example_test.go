7e150ad7d43a0275e34e3d55fb12f3567d693f0d
Adrian-George Bostan <adrg@epistack.com>
2019-12-13 12:24:42 +0200

    Add example tests for n-gram related functions

--- example_test.go
+++ example_test.go
@@ -39,3 +39,35 @@ func ExampleSliceContains() {
 	// ([a b c], b): true
 	// ([a b c], d): false
 }
+
+func ExampleNgrams() {
+	fmt.Println("abbcd n-grams (size 2):", strutil.Ngrams("abbcd", 2))
+	fmt.Println("abbcd n-grams (size 3):", strutil.Ngrams("abbcd", 3))
+
+	// Output:
+	// abbcd n-grams (size 2): [ab bb bc cd]
+	// abbcd n-grams (size 3): [abb bbc bcd]
+}
+
+func ExampleNgramMap() {
+	// 2 character n-gram map.
+	ngrams, total := strutil.NgramMap("abbcabb", 2)
+	fmt.Printf("abbcabb n-gram map (size 2): %v (%d ngrams)\n", ngrams, total)
+
+	// 3 character n-gram map.
+	ngrams, total = strutil.NgramMap("abbcabb", 3)
+	fmt.Printf("abbcabb n-gram map (size 3): %v (%d ngrams)\n", ngrams, total)
+
+	// Output:
+	// abbcabb n-gram map (size 2): map[ab:2 bb:2 bc:1 ca:1] (6 ngrams)
+	// abbcabb n-gram map (size 3): map[abb:2 bbc:1 bca:1 cab:1] (5 ngrams)
+}
+
+func ExampleNgramIntersection() {
+	ngrams, common, total := strutil.NgramIntersection("ababc", "ababd", 2)
+	fmt.Printf("(ababc, ababd) n-gram intersection: %v (%d/%d n-grams)\n",
+		ngrams, common, total)
+
+	// Output:
+	// (ababc, ababd) n-gram intersection: map[ab:2 ba:1] (3/8 n-grams)
+}
