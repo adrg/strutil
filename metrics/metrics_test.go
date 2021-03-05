aa4f4b6aa3ea0e239658c910d626fdf57a9265cd
Adrian-George Bostan <adrg@epistack.com>
2021-03-05 09:44:08 +0200

    Add Jaccard test case

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -1,17 +1,33 @@
 package metrics_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/adrg/strutil/metrics"
 	"github.com/stretchr/testify/require"
 )
 
+func sf(a float64) string {
+	return fmt.Sprintf("%.2f", a)
+}
+
 func TestHamming(t *testing.T) {
 	h := metrics.NewHamming()
-	require.Zero(t, h.Distance("", ""))
-	require.Equal(t, 0.75, h.Compare("text", "test"))
-	require.Equal(t, 0.5, h.Compare("once", "one"))
+	require.Equal(t, 0, h.Distance("", ""))
+	require.Equal(t, "0.75", sf(h.Compare("text", "test")))
+	require.Equal(t, "0.50", sf(h.Compare("once", "one")))
 	h.CaseSensitive = false
-	require.Equal(t, 0.5, h.Compare("one", "ONCE"))
+	require.Equal(t, "0.50", sf(h.Compare("one", "ONCE")))
+}
+
+func TestJaccard(t *testing.T) {
+	j := metrics.NewJaccard()
+	require.Equal(t, "1.00", sf(j.Compare("", "")))
+	require.Equal(t, "0.43", sf(j.Compare("night", "alright")))
+	j.NgramSize = 0
+	require.Equal(t, "0.43", sf(j.Compare("night", "alright")))
+	j.CaseSensitive = false
+	j.NgramSize = 3
+	require.Equal(t, "0.33", sf(j.Compare("NIGHT", "alright")))
 }
